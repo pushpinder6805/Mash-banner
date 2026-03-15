d9608b923db56b76ce1a848a973a616e643c6089
Pushpender Singh <52063297+pushpinder6805@users.noreply.github.com>
2026-03-15 14:48:22 +0530

    Updated homepage-search-banner.js

--- javascripts/discourse/connectors/above-main-container/homepage-search-banner.js
+++ javascripts/discourse/connectors/above-main-container/homepage-search-banner.js
@@ -1,28 +1,5 @@
-import { service } from "@ember/service";
-
 export default {
-  router: service(),
-
   setupComponent(args, component) {
-    const updateBanner = () => {
-      const router = this.router;
-
-      if (!router) {
-        this.set("showBanner", false);
-        return;
-      }
-
-      const route = router.currentRouteName;
-
-      this.set("showBanner",
-        route === "discovery.latest" ||
-        route === "discovery.categories" ||
-        route === "discovery.top"
-      );
-    };
-
-    updateBanner();
-
-    this.router.on("routeDidChange", updateBanner);
+    component.set("showBanner", true);
   },
 };
